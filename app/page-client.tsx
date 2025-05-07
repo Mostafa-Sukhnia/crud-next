09c2e01e47ff429edf05cb61c4ad5fe1af1cf9e1
Mustafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2025-05-08 00:40:25 +0300

    feat: enhance user management with CRUD operations and update schema

--- app/page-client.tsx
+++ app/page-client.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import React, { useActionState } from "react";
-import { addUser, getServerResponse } from "./user-action";
+import React, { useActionState, useState, useEffect } from "react";
+import {
+  addUser,
+  getServerResponse,
+  getUsers,
+  findUser,
+  delUser,
+  updateUser,
+} from "./user-action";
+import { User } from "./Types";
 
 export default function PageClient() {
   const [message1, action1] = useActionState(async () => {
@@ -13,6 +21,39 @@ export default function PageClient() {
     const email = formData.get("email") as string;
     return await addUser({ name, email });
   }, "");
+
+  const [message3, action3] = useActionState(async (prev, formData) => {
+    const id = formData.get("id") as string;
+    if (!id) return "ID is required";
+
+    const user: any = await findUser(Number(id));
+    if (!user) return "User not found";
+    else return user;
+  }, "");
+
+  const [message4, action4] = useActionState(async (prev, formData) => {
+    updateUser(Number(formData.get("id")), formData.get("name") as string);
+    return "User updated";
+  }, "");
+
+  const [deleteMessage, setDeleteMessage] = useState(""); // لحفظ رسالة الحذف
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = async () => {
+    const data = await getUsers();
+    setUsers(data);
+  };
+
+  const handleDelete = async (id: number) => {
+    await delUser(id);
+    setDeleteMessage("User Deleted");
+
+    await fetchUsers(); // تحديث قائمة المستخدمين
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <div className="flex flex-col gap-4 p-6">
@@ -41,6 +82,103 @@ export default function PageClient() {
         </button>
       </form>
       <div>{message2}</div>
+
+      <table className="border-2 border-black rounded-xl p-3">
+        <thead>
+          <tr>
+            <th className="border-2 border-black rounded-xl p-3 text-start">
+              ID
+            </th>
+            <th className="border-2 border-black rounded-xl p-3 text-start">
+              Name
+            </th>
+            <th className="border-2 border-black rounded-xl p-3 text-start">
+              Email
+            </th>
+            <th className="border-2 border-black rounded-xl p-3 text-start">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td className="border-2 border-black p-3 text-start">
+                {user.id}
+              </td>
+              <td className="border-2 border-black p-3 text-start">
+                {user.name}
+              </td>
+              <td className="border-2 border-black p-3 text-start">
+                {user.email}
+              </td>
+              <td className="border-2 border-black p-3 text-start">
+                <button
+                  onClick={() => handleDelete(user.id)}
+                  className="bg-red-500 text-white rounded px-3 py-1 cursor-pointer hover:bg-red-600"
+                >
+                  Delete User
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {deleteMessage && (
+        <div className="text-red-500 font-semibold">{deleteMessage}</div>
+      )}
+
+      <div className="flex flex-col gap-4 p-6">
+        <p className="bg-green-500 p-2 m-2">Find user</p>
+        <form action={action4}>
+          <input
+            type="number"
+            name="id"
+            placeholder="ID"
+            className="border-2 border-black rounded-xl p-3"
+          />
+          <button type="submit" className="bg-green-300 rounded-xl p-3">
+            Find User
+          </button>
+          {typeof message3 === "string" ? (
+            <p>{message3}</p>
+          ) : (
+            <div className="border-2 border-black rounded-xl p-3">
+              <p>
+                <strong>ID:</strong> {message3.id}
+              </p>
+              <p>
+                <strong>Name:</strong> {message3.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {message3.email}
+              </p>
+            </div>
+          )}
+        </form>
+        <div>
+          <div>update user</div>
+          <form action={action4}>
+            <input
+              type="number"
+              name="id"
+              placeholder="ID"
+              className="border-2 border-black rounded-xl p-3"
+            />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="border-2 border-black rounded-xl p-3"
+            />
+            <button type="submit" className="bg-green-300 rounded-xl p-3">
+              Update User
+            </button>
+            <div>{message4}</div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- app/user-action.ts
+++ app/user-action.ts
@@ -3,20 +3,54 @@ import { PrismaClient } from "../app/generated/prisma";
 
 import { User } from "./Types";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const getServerResponse = async () => {
-    return ("Server Action Called")
-}
+  return "Server Action Called";
+};
 
-export const addUser = async (dataOfUser:User) => {
-    await prisma.user.create({
-        data:{
-            name: dataOfUser.name,
-            email: dataOfUser.email,
-            createsAt: new Date(),
-            updatedAt: new Date()
-        }
-    })
-    return ("User Added")
-}
+/**
+ * * @param dataOfUser - The user data to be added.
+ * @param dataOfUser.name - The name of the user.
+ * * @param dataOfUser.email - The email of the user.
+ * @returns A promise that resolves to a string indicating the result of the operation.
+ */
+export const addUser = async (dataOfUser: User) => {
+  await prisma.user.create({
+    data: {
+      name: dataOfUser.name,
+      email: dataOfUser.email,
+      createsAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+  return "User Added";
+};
+export const getUsers = async () => {
+  return await prisma.user.findMany();
+};
+export const updateUser = async (id: number, name: string) => {
+  return (
+    await prisma.user.update({
+      where: { id },
+      data: {
+        name: name,
+        updatedAt: new Date(),
+      },
+    }),
+    "User updated"
+  );
+};
+export const delUser = async (id: number) => {
+  return (
+    await prisma.user.delete({
+      where: { id },
+    }),
+    "User Deleted"
+  );
+};
+export const findUser = async (id: number) => {
+  return await prisma.user.findUnique({
+    where: { id },
+  });
+};
